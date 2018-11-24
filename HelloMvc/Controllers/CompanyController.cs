38b74644865a3e168e486696c4879ced103ed59c
Parth Sharma <parth.sharma@trios.com>
2018-11-23 17:44:51 -0800

    Introduction to MVC

---
+++ HelloMvc/Controllers/CompanyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMvc
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult  Index()
+        {
+            return View();
+        }
+
+        public ActionResult Leadership()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ HelloMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloMvc.Startup))]
+namespace HelloMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
